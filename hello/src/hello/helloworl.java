f7c53f3d1530f27c570d2a6ee6eaa9210cac11c9
chi <dangchidv96@gmail.com>
2019-04-11 02:55:48 +0700

    demo git eclipse

---
+++ hello/src/hello/helloworl.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class helloworl {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
